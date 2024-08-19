ed2e21ffabc7efea38bddca231e2dcec27f0e8ba
kashiarain <kashiarain2000@gmail.com>
2024-08-19 20:37:24 +0500

    updated

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
-const mode = document.querySelector('#mode')
-mode.onclick = () => document.documentElement.classList.toggle('dark')
+const themeToggle = document.getElementById("theme-toggle");
+console.log(themeToggle.value);
+themeToggle.onchange = () => {
+    console.log(themeToggle.checked);
+};
+
+// Check for saved user preference, if any, on load
+if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+    document.documentElement.classList.add("dark");
+} else {
+    if (themeToggle.value) themeToggle.click();
+}
+
+// Listen for a click on the toggle button
+themeToggle.onchange = () => {
+    // If set via local storage previously
+    if (localStorage.getItem("color-theme")) {
+        if (localStorage.getItem("color-theme") === "light") {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        }
+
+        // If NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        }
+    }
+};
